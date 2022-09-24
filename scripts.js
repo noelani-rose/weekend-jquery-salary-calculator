71f2a29247d203ded0eef68a133d16faa021a820
Noelani West <noelani.rose14@gmail.com>
2022-09-24 03:15:33 -0500

    created functions with only console logs

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 $(document).ready(onReady);
 
+
+let employeeInfo = [];
+let totalMonthly = 0;
+
 function onReady() {
     console.log('in onReady')
-    // $('#messageForm').on('submit', onSubmitPost);
+    $('#employeeForm').on('submit', submitBtn);
     // // console.log('in onReady', $('.deleteBtn'));
     // // $('.deleteBtn').on('click', onDelete);
     // // Listen for clicks anywhere on the table
     // // but only call onDelete if it's a `.deleteBtn` element
     // $('#postsTable').on('click', '.deleteBtn', onDelete);
+    $('#postsTable').on('click', '.deleteBtn', deleteBtn);
+}
+
+function submitBtn(evt){
+    evt.preventDefault();
+    console.log('in submitBtn');
+
+}
+
+function deleteBtn(){
+    console.log('in deleteBtn')
+}
+
+function render(){
+    console.log('in render');
 }
